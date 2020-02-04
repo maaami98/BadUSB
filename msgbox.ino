d621517e8bd8d67402d0682cf6b21a59fc9ba539
maaami98 <35247648+maaami98@users.noreply.github.com>
2020-02-05 00:49:05 +0300

    Update msgbox.ino

--- msgbox.ino
+++ msgbox.ino
@@ -1,6 +1,5 @@
 #include "DigiKeyboard.h"
 void setup() {
-  // don't need to set anything up to use DigiKeyboard
   DigiKeyboard.sendKeyStroke(0);
   DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
   DigiKeyboard.delay(100);
